ac623902cdd88b572a9a2a3e817db8e0aa83edba
lulu <862055705@qq.com>
2022-10-26 00:48:44 +0800

    Request请求优化
    1.更改了文件目录
    2.优化了request请求容错
    3.增加了多种登录情况的测试与鲁棒性
    4.继续向屎山方向发展

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 App({
   onLaunch() {
     
+
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -10,136 +11,145 @@ App({
 
     // 登录
     wx.login({
-        success: res => {
-          // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        }
-      })
-    
-   
- 
-    
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
 
-  },
-  onShow(){
-    var that=this;
+    var that = this;
     //上来请求数据
-    if(wx.getStorageSync('islogin')==true){
-    
+    if (wx.getStorageSync('islogin') == true) {
+      //get_login_info
       wx.request({
         url: 'http://127.0.0.1:5000/get_login_info',
-        method:'POST',
-        data:{
-          account:wx.getStorageSync('useraccount'),
-          password:wx.getStorageSync('userpws'),
+        method: 'POST',
+        data: {
+          account: wx.getStorageSync('useraccount'),
+          password: wx.getStorageSync('userpws'),
         },
-  
-        header:{
-          'content-type':'application/json'      
+
+        header: {
+          'content-type': 'application/json'
           //后端生成cookie然后请求的时候把cookie发过去，然后我们进行加工。
         },
         success: (res) => {
           // 将用户的cookie存入至本地
-          console.log(res.data)
-          wx.setStorageSync('cookiesstr', res.data); 
+          wx.setStorageSync('cookiesstr', res.data);
         },
         fail: (res) => {
           //清楚登录状态
           console.log(res.data)
-          wx.setStorageSync('islogin',false); 
+          wx.setStorageSync('islogin', false);
         }
-
       })
       //请求课表数据
-      if(wx.getStorageSync('islogin')==true){
-      wx.request({
-        url: 'http://127.0.0.1:5000/get_class_info',
-        method:'POST',
-        data:{
-          account:wx.getStorageSync('useraccount'),
-          password:wx.getStorageSync('userpws'),
-          cookiesstr:wx.getStorageSync('cookiesstr')
-        },
-  
-        header:{
-          'content-type':'application/json'      
-          //后端生成cookie然后请求的时候把cookie发过去，然后我们进行加工。
-        },
-        success: (res) => {
-          // 将课表传输到schedule_table
-          that.globalData.class_info=res.data
-        },
-        fail: (res) => {
-          //清楚登录状态
-          console.log(res.data)
-          wx.setStorageSync('islogin',false); 
 
-        }
+      if (wx.getStorageSync('islogin') == true) {
+        wx.request({
+          url: 'http://127.0.0.1:5000/get_class_info',
+          method: 'POST',
+          data: {
+            account: wx.getStorageSync('useraccount'),
+            password: wx.getStorageSync('userpws'),
+            cookiesstr: wx.getStorageSync('cookiesstr')
+          },
 
-      })
-      //请求学生信息
-      wx.request({
-        url: 'http://127.0.0.1:5000/get_student_info',
-        method:'POST',
-        data:{
-          account:wx.getStorageSync('useraccount'),
-          password:wx.getStorageSync('userpws'),
-          cookiesstr:wx.getStorageSync('cookiesstr')
-        },
-  
-        header:{
-          'content-type':'application/json'      
-        },
-        success: (res) => {
-          that.globalData.student_info=res.data
-        },
-        fail: (res) => {
-          //清楚登录状态
-          console.log(res.data)
-          wx.setStorageSync('islogin',false); 
-        }
+          header: {
+            'content-type': 'application/json'
+            //后端生成cookie然后请求的时候把cookie发过去，然后我们进行加工。
+          },
+          success: (res) => {
+            // 将课表传输到schedule_table
+            if (res.data["token"] == "-1") {
+              wx.setStorageSync('islogin', false);
+            }
+            that.globalData.class_info = res.data
 
-      })
-      //请求时间信息
-      wx.request({
-        url: 'http://127.0.0.1:5000/get_current_time',
-        method:'POST',
-        data:{
-          account:wx.getStorageSync('useraccount'),
-          password:wx.getStorageSync('userpws'),
-          cookiesstr:wx.getStorageSync('cookiesstr')
-        },
-        header:{
-          'content-type':'application/json'      
-        },
-        success: (res) => {
-          that.globalData.current_time=res.data
-          that.globalData.week_time=res.data["zc"]
+          },
+          // fail: (res) => {
+          //   //清楚登录状态
+          //   console.log(res.data)
+          //   wx.setStorageSync('islogin',false); 
 
-        },
-        fail: (res) => {
-          //清楚登录状态
-          console.log(res.data)
-          wx.setStorageSync('islogin',false); 
-        }
+          // }
 
-      })
+        })
+        //请求学生信息
+        wx.request({
+          url: 'http://127.0.0.1:5000/get_student_info',
+          method: 'POST',
+          data: {
+            account: wx.getStorageSync('useraccount'),
+            password: wx.getStorageSync('userpws'),
+            cookiesstr: wx.getStorageSync('cookiesstr')
+          },
+
+          header: {
+            'content-type': 'application/json'
+          },
+          success: (res) => {
+            if (res.data["token"] == "-1") {
+              wx.setStorageSync('islogin', false);
+            }
+            that.globalData.student_info = res.data
+          },
+          // fail: (res) => {
+          //   //清楚登录状态
+          //   console.log(res.data)
+          //   wx.setStorageSync('islogin',false); 
+          // }
+
+        })
+        //请求时间信息
+        wx.request({
+          url: 'http://127.0.0.1:5000/get_current_time',
+          method: 'POST',
+          data: {
+            account: wx.getStorageSync('useraccount'),
+            password: wx.getStorageSync('userpws'),
+            cookiesstr: wx.getStorageSync('cookiesstr')
+          },
+          header: {
+            'content-type': 'application/json'
+          },
+          success: (res) => {
+            that.globalData.current_time = res.data
+            that.globalData.week_time = res.data["zc"]
+            wx.setStorageSync('zc', that.globalData.week_time);
+
+          },
+          // fail: (res) => {
+          //   //清楚登录状态
+          //   console.log(res.data)
+          //   wx.setStorageSync('islogin',false); 
+          //   that.globalData.week_time=wx.getStorageSync('zc')//增加读取失败的反误差
+          // }
+
+        })
+
+      }
 
     }
-  
-  }
+
+
+
+
+  },
+  onShow() {
+
 
   },
   globalData: {
     userInfo: null,
-    class_info:null,//课程信息
-    student_info:null,//学生信息
-    current_time:null,//时间信息
-    week_time:'N',    //第几周
-    set_all_data:{
-      isbindshareflag:true,                        //是否绑定分享
-      isshareshow:true,                            //是否显示分享
-      islogin:wx.getStorageSync('islogin')         //是否绑定课表
+    class_info: null, //课程信息
+    student_info: null, //学生信息
+    current_time: null, //时间信息
+    week_time: 0, //第几周
+    set_all_data: {
+      isbindshareflag: true, //是否绑定分享
+      isshareshow: true, //是否显示分享
+      islogin: wx.getStorageSync('islogin') //是否绑定课表
     }
   }
-  
+
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -16,6 +16,9 @@
   -？session有问题，session在python是建立契约的方式，一个python程序只能带一个session。
      session不是cookie，但session中好像携带看看能不能实现。
 5.session一下把所有数据都拿来，不如2。
+
+血的教训
+请求失败是前端请求失败！就算我返回404他也是属于请求成功的。
   */
 Page({
 
@@ -23,133 +26,151 @@ Page({
    * 页面的初始数据
    */
   data: {
-    useraccount:'',//用户账户
-    userpws:'',//用户密码
-    isshow:false,//是否显示密码
-    _src:'/static/image/hidepws.png',//隐藏的图片，初始均为不可见
-    islogin:true,//是否登录
-    istemp:false,//账号验证是否正确
+    useraccount: '', //用户账户
+    userpws: '', //用户密码
+    isshow: false, //是否显示密码
+    _src: '/static/image/hidepws.png', //隐藏的图片，初始均为不可见
+    islogin: true, //是否登录
+    
   },
-    //将账号和密码进行传参到后端，返回值为ispermit,判断是否允许
-  getdatalist(){
-    let that=this;
+  //将账号和密码进行传参到后端，返回值为ispermit,判断是否允许
+  getdatalist() {
+    var that = this;
+
+    
+
+  },
+
+  //进行登录的设置
+  loginTo() {
+    var that = this;
+    let {
+      useraccount,
+      userpws
+    } = this.data;
+
+    // 前端验证
+    if (!useraccount) {
+      wx.showToast({
+        title: '学号不能为空',
+        icon: 'none'
+      })
+      
+      return;
+    }
+    let phoneReg = /^\d{12}$/;
+    if (!phoneReg.test(useraccount)) {
+      wx.showToast({
+        title: '学号格式错误',
+        icon: 'none'
+      })
+      this.setData({
+        userpws: '',
+        useraccount: ''
+      });
+      return;
+    }
+    if (!userpws) {
+      wx.showToast({
+        title: '密码不能为空',
+        icon: 'none'
+      })
+      return;
+    }
+    
+    //后端鉴权
+    //后端鉴权有个核心问题，你没办法保证你的你在规定时间里获得request里的信息。
+    //request的函数是回调函数
     wx.request({
       url: 'http://127.0.0.1:5000/get_login_info',
-      method:'POST',
-      data:{
-        account:this.data.useraccount,
-        password:this.data.userpws
+      method: 'POST',
+      data: {
+        account: that.data.useraccount,
+        password: that.data.userpws
       },
-      header:{
-        'content-type':'application/json'      
+      header: {
+        'content-type': 'application/json'
         //后端生成cookie然后请求的时候把cookie发过去，然后我们进行加工。
       },
       success: (res) => {
-        // 将用户的cookie存入至本地
+        if(res.data["code"]==2002){
+          wx.setStorageSync('islogin',false);
+          console.log("请求失败了~")
+        }
+          // 将用户的cookie存入至本地
+         
+         else{
+
         wx.setStorageSync('cookiesstr', res.data);
-        console.log(res.data);
-        that.setData({
-          istemp:true
-        })
+        wx.setStorageSync('islogin',true);
       }
-      
+      var status=wx.getStorageSync('islogin');
+      console.log("请求了~")
+      //登录成功
+      if (status) {
+        wx.showToast({
+          title: '登录成功',
+          icon: "success"
+        });
+        that.setData({
+          islogin: true
+        })
+        wx.setStorageSync('useraccount', this.data.useraccount);
+        wx.setStorageSync('userpws', this.data.userpws);
+        console.log(wx.getStorageSync('islogin'))
+        wx.switchTab({
+          url: '../Menu/Home/home',
+        })
+
+      }
+      //密码或账号错误
+      else {
+        wx.showToast({
+          title: '账号或密码错误',
+          icon: "error"
+        });
+        this.setData({
+          userpws: ''
+          
+        });
+      }
+
+
+
+        
+      }
+
     })
     
-  },
+
     
-  //进行登录的设置
-  loginTo(){
-    var that=this;
-    let {useraccount, userpws} = this.data;
-        // 前端验证
-        if(!useraccount){
-          wx.showToast({
-            title: '学号不能为空',
-            icon: 'none'
-          })
-          return;
-        }
-        let phoneReg = /^\d{12}$/;
-        if(!phoneReg.test(useraccount)){
-          wx.showToast({
-            title: '学号格式错误',
-            icon: 'none'
-          })
-          return;
-        }
-        if(!userpws){
-          wx.showToast({
-            title: '密码不能为空',
-            icon: 'none'
-          })
-          return;
-        }
-      
-        var status;
-        that.getdatalist(); //后端判定
-        setTimeout(()=>
-        {
-          status=this.data.istemp;
-          //登录成功
-          if(status){
-            wx.showToast({
-              title: '登录成功',
-              icon: "success"
-            });
-            this.setData({
-              islogin:true
-            }) 
-            wx.setStorageSync('useraccount', this.data.useraccount);
-            wx.setStorageSync('userpws', this.data.userpws);
-            wx.setStorageSync('islogin', this.data.islogin);
-            wx.switchTab({
-              url: '../home/home',
-            });
-        
-          }
-          //密码或账号错误
-          else{
-            wx.showToast({
-              title: '账号或密码错误',
-              icon: "error"
-            });
-            this.setData({
-              userpws:'',
-              useraccount:''
-            });
-          }
-         
-          
-          
-        }, 100)
-      
+
   },
-  
+
   // 改变密码状态
-  changeshow(){
-    if(this.data.isshow){
+  changeshow() {
+    if (this.data.isshow) {
       this.setData({
-        _src:'/static/image/hidepws.png',
-        isshow:false
+        _src: '/static/image/hidepws.png',
+        isshow: false
       })
-    }
-    else{
+    } else {
       this.setData({
-        _src:'/static/image/showpws.png',
-        isshow:true
+        _src: '/static/image/showpws.png',
+        isshow: true
       })
     }
   },
   //获取账号名
-  getaccount(e){
+  getaccount(e) {
     this.setData({
-      useraccount:e.detail.value
+      useraccount: e.detail.value
     })
   },
   //获取密码
-  getpassword(e){
+  getpassword(e) {
     this.setData({
-      userpws:e.detail.value
+      userpws: e.detail.value
     })
   },
   // //忘记密码跳转
@@ -158,20 +179,20 @@ Page({
   //     url: '/pages/login/tiaozhuan',
   //   })
   // },
-  
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-        //页面加载时，从微信缓存读取账号密码
-        var _this=this;
-        _this.setData({
-          useraccount:wx.getStorageSync('useraccount'),
-          userpws:wx.getStorageSync('userpws'),
-          islogin:wx.getStorageSync('islogin')
-        })
+    //页面加载时，从微信缓存读取账号密码
+    var _this = this;
+    _this.setData({
+      useraccount: wx.getStorageSync('useraccount'),
+      userpws: wx.getStorageSync('userpws'),
+      islogin: wx.getStorageSync('islogin')
+    })
   },
- 
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -183,8 +204,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-  
-    
+
+
   },
 
   /**
